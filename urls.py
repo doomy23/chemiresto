6dacaf597a9ac474ea5072a28d8751247d421cf9
Doomy23 <doomy23@gmail.com>
2015-05-05 22:35:19 -0400

    Modifications and new basic stuff we will need later

--- urls.py
+++ urls.py
@@ -4,9 +4,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
-	url(r'^admin/', include(admin.site.urls))
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', TemplateView.as_view(template_name="index.html"), name="home")
 )
 
 if settings.DEBUG:
