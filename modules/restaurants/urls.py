0555d86692da4fe08c88fa39abd523985543608f
Dominic Roberge <doomy23@gmail.com>
2015-07-10 15:38:05 -0400

    FINI x 2

--- modules/restaurants/urls.py
+++ modules/restaurants/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, url
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^list/$', RestaurantsView.as_view(), name="restaurants_list"),
-    url(r'^(?P<id>.+)/$', RestaurantsDetailsView.as_view(), name="restaurants_details")
+    url(r'^(?P<id>.+)/$', RestaurantsDetailsView.as_view(), name="restaurants_details"),
+    url(r'^$', RestaurantsView.as_view(), name="restaurants_list")
 )
