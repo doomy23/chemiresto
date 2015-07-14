db72a1a70d9d1bb4d229cab4a63c4b17baa7b398
anthony-mn-coallier <anthony.mn.coallier@gmail.com>
2015-07-14 13:30:39 -0400

    Utilisation de fixture (c'est moins chiant que gosser dans BD

--- manage.py
+++ manage.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import sys
 import dotenv
-import shutil
-from getenv import env
 
 if __name__ == "__main__":
     dotenv.read_dotenv()
-    
-    if not os.path.isfile(env('SQLITE_DB')) and sys.argv[1] == "runserver":
-        print "Copying chemiresto.prod.sqlite to chemiresto.sqlite...\n"
-        shutil.copy2(env('SQLITE_DB_BACKUP'), env('SQLITE_DB'))
-        
-        print "Adding recently created models...\n"
-        os.system('manage.py syncdb')
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
